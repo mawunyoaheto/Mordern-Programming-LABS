bb685f4495d15ab87473e3d65466f5caa1b477de
Raymond Mawunyo Aheto <raymondaheto@gmail.com>
2021-11-12 11:19:16 -0600

    added method to handle generation of squares

--- lesson9/labs/pob9/PrintSquares.java
+++ lesson9/labs/pob9/PrintSquares.java
@@ -3,22 +3,25 @@ package lesson9.labs.pob9;
 import java.util.stream.IntStream;
 
 public class PrintSquares {
-	private static int num = 1;
-	
 	
 	public static void main(String[] args) {
 		
-		printSquares(20);
+		printSquares(4);
 	}
 
 	public static void printSquares(int n) {
 		 	
-		
-	IntStream.iterate(1,x->{
-		num += 1;
-		return num * num;
-	}).limit(n).forEach(val->System.out.println(val));
-	
+
+		IntStream.iterate(1,x->genNextSquare(x))
+		.limit(n)
+		.forEach(val->System.out.println(val));
+
 	}
+
+	
+	 public static int genNextSquare(int n) {
+	        n = (int) Math.floor(Math.sqrt(n)) + 1;
+	        return n * n;
+	    }
 
 }
